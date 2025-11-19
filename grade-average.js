1fb046ebbfcf2d456db700642d725f01c5f22080
jbrackin1 <92929373+jbrackin1@users.noreply.github.com>
2025-11-19 02:13:44 +0000

    first commit

---
+++ grade-average.js
@@ -0,0 +1,8 @@
+function calculateAverage() {
+    const grade1 = parseFloat(document.getElementById('grade1').value);
+    const grade2 = parseFloat(document.getElementById('grade2').value);
+    const grade3 = parseFloat(document.getElementById('grade3').value);
+
+    const average = (grade1 + grade2 + grade3) / 3;
+    document.getElementById('result').innerText = `Average Grade: ${average.toFixed(2)}`;
+}
